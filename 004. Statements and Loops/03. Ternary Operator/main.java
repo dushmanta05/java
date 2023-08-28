6f2d5276daec6db10642a9a8336de2efde815959
Dushmanta Behera <dushmanta.dev@gmail.com>
2023-08-28 23:41:10 +0530

    finish Ternary operator & switch case

---
+++ 004. Statements and Loops/03. Ternary Operator/main.java
@@ -0,0 +1,10 @@
+class ternary{
+    
+    public static void main ( String a[]){
+        int x = 7;
+        boolean result;
+
+        result = (x % 2 == 0) ? true : false;
+        System.out.println((result));
+    }
+}
